1091da4de81e0c9de109199a304b46d0fe358696
harrandt <116627373+harrandt@users.noreply.github.com>
2023-08-22 15:37:34 +0200

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { appRouter } from './app-router';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '../lib/shared/query-client';
+import { queryClient } from 'lib/shared/query-client';
 import '../i18n';
 import { HarrandtThemeProvider } from '@oh/shared/theme';
 
